15537705fea3b37f9d28ab429a124352eaefe9c8
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 13:01:52 -0700

    Added remove_function - further study

--- wordcount.py
+++ wordcount.py
@@ -7,21 +7,33 @@
 # one function that prints out each occurence of the word in the file
 # close file
 
+def remove_punctuation(word):
+    punctuation = ".,?"
+    string = ""
+    for letter in word:
+        if letter not in punctuation:
+            string = letter + string 
+    return string[::-1]
+
+
 def word_count(file_name):
-
-    word_file = open(file_name)
     word_count = {}
+    with open(file_name) as word_file:
+    
 
-    for line in word_file:
-        line = line.rstrip()
-        words = line.split(' ')
+        for line in word_file:
+            line = line.rstrip()
+            words = line.split(' ')
 
-        for word in words:
-            word_count[word] = word_count.get(word, 0) + 1
-            
+            for word in words:                
+                word = remove_punctuation(word)
+                word = word.lower()
+                word_count[word] = word_count.get(word, 0) + 1
+                
 
-    for word, number in word_count.iteritems():
-        print "{} {}".format(word, number)
+        for word, number in word_count.iteritems():
+            print "{} {}".format(word, number)
 
 
 word_count("test.txt")
+
